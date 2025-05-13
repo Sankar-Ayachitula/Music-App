30f303e2e9ff08d1721d1285a317e6aacb0bb876
Sankar Ayachitula <sankar.ayachitula@bajajfinserv.in>
2025-05-14 04:32:04 +0530

    feat: Add Song Data Model and Static Song List
    - Created a `Song` data class to represent song information with title, artist, album, and duration.
    - Added `StaticData` object containing a predefined list of `Song` objects.

---
+++ app/src/main/java/com/example/musicpocapp/Pojos/Song.kt
@@ -0,0 +1,3 @@
+package com.example.musicpocapp.Pojos
+
+data class Song(val title: String, val artist: String, val album: String, val duration: String)
---
+++ app/src/main/java/com/example/musicpocapp/utilities/StaticData.kt
@@ -0,0 +1,21 @@
+package com.example.musicpocapp.utilities
+
+import com.example.musicpocapp.Pojos.Song
+
+object StaticData {
+    val songs = listOf(
+        Song("Song 1", "Artist 1", "Album 1", "3:45"),
+        Song("Song 2", "Artist 2", "Album 2", "3:45"),
+        Song("Song 3", "Artist 3", "Album 3", "3:45"),
+        Song("Song 4", "Artist 4", "Album 4", "3:45"),
+        Song("Song 5", "Artist 5", "Album 5", "3:45"),
+        Song("Song 6", "Artist 6", "Album 6", "3:45"),
+        Song("Song 7", "Artist 7", "Album 7", "3:45"),
+        Song("Song 1", "Artist 1", "Album 1", "3:45"),
+        Song("Song 2", "Artist 2", "Album 2", "3:45"),
+        Song("Song 3", "Artist 3", "Album 3", "3:45"),
+        Song("Song 4", "Artist 4", "Album 4", "3:45"),
+        Song("Song 5", "Artist 5", "Album 5", "3:45"),
+        Song("Song 6", "Artist 6", "Album 6", "3:45"),
+        Song("Song 7", "Artist 7", "Album 7", "3:45"))
+}
